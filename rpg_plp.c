da650fbe0b20b449fb28d5decfb51fec8bed2769
Rafael de Padua Oliveira <102425973+RfaelPadua@users.noreply.github.com>
2024-10-13 13:28:08 -0300

    .

--- rpg_plp.c
+++ rpg_plp.c
@@ -9,15 +9,31 @@
 
 typedef struct Character{
     char name[20];
-    int class;             
+    char body1[20];
+    char body2[20];
+    char body3[20];
+    char class[10];
     int health;
     int stamina;           
     int attack;              
     int defense;            
     int speed;    
 } Character;
+
+typedef struct Enemy{
+    char enemy[20];
+    char body[20];
+    int health;
+    int attack;
+    int defense;
+    int speed;
+} Enemy;
+
+
 // --------------------------------------------------------------
 // VISUAL FUNCTIONS PREVIEW
+
+
 
 void NameScreen(void);
 void Menu(void);
@@ -33,12 +49,29 @@ void PauseScreen(void);
 void Knight(Character* P);
 void Assassin(Character* P);
 void Archer(Character* P);
-// --------------------------------------------------------------  
+// --------------------------------------------------------------
+// ENEMY FUNCTIONS PREVIEW
+
+void Slime(Enemy* E);
+void Skeleton(Enemy* E);
+void Goblin(Enemy* E);
+void Demon(Enemy* E);
+// --------------------------------------------------------------
+// COMBAT FUNCTIONS PREVIEW
+
+void BattleLayout(Character* P, Enemy* E);
+
+
+
 
 int main(void) {
     char select = 'Z';
     Character C;
-    C.class = -1;
+    system("chcp 65001 > nul");
+    int control = 0;
+
+
+    PauseScreen();
 
     // Choose the player name
     do{
@@ -62,23 +95,30 @@ int main(void) {
                 Knight(&C);
                 KMS();
                 printf("\n\t\t\t\t\tYou've choosen Knight\n");
+                control = 1;
                 break;
             case 'S':
                 Assassin(&C);
                 SMS();
                 printf("\n\t\t\t\t\tYou've choosen Assassin\n");
+                control = 1;
                 break;
             case 'A':
                 Archer(&C);
                 AMS();
                 printf("\n\t\t\t\t\tYou've choosen Archer\n");
+                control = 1;
                 break;
             default:
                 printf("\n\t\t\t\t\tNot a valid option\n");
                 break;
         }
         PauseScreen();
-    }while(C.class == -1);
+    }while(control == 0);
+    
+
+
+
 
     return 0;
 }
@@ -162,7 +202,10 @@ void PauseScreen(void) {
 
 // Gives the knight attributes
 void Knight(Character* P) {
-    P->class = 1;
+    strcpy(P->body1, "   O   ");
+    strcpy(P->body2, "U/ | \\P");
+    strcpy(P->body3, "  / \\  ");
+    strcpy(P->class, "Knight");
     P->health = 65;
     P->stamina = 35;
     P->attack = 50;
@@ -171,7 +214,10 @@ void Knight(Character* P) {
 }
 // Gives the assassin attributes
 void Assassin(Character* P) {
-    P->class = 2;
+    strcpy(P->body1, "   O   ");
+    strcpy(P->body2, "i/ | \\i");
+    strcpy(P->body3, "  / \\  ");
+    strcpy(P->class, "Assassin");
     P->health = 35;
     P->stamina = 50;
     P->attack = 80;
@@ -180,12 +226,71 @@ void Assassin(Character* P) {
 }
 // Gives the archer attributes
 void Archer(Character* P) {
-    P->class = 3;
+    strcpy(P->body1, "   O   ");
+    strcpy(P->body2, "|/ | \\D");
+    strcpy(P->body3, "  / \\  ");
+    strcpy(P->class, "Archer");
     P->health = 35;
     P->stamina = 50;
     P->attack = 50;
     P->defense = 20;
     P->speed = 80;
+}
+
+// --------------------------------------------------------------
+// ENEMY FUNCTIONS PREVIEW
+
+// Gives the slime attributes
+void Slime(Enemy* E) {
+    strcpy(E->enemy, "Slime");
+    strcpy(E->body, "\U0001F4A9");
+    E->health = 20;
+    E->attack = 10;
+    E->defense = 10;
+    E->speed = 10;
+}
+
+// Gives the skeleton attributes
+void Skeleton(Enemy* E) {
+    strcpy(E->enemy, "Skeleton");
+    E->health = 30;
+    E->attack = 15;
+    E->defense = 15;
+    E->speed = 15;
+}
+
+// Gives the goblin attributes
+void Goblin(Enemy* E) {
+    strcpy(E->enemy, "Goblin");
+    E->health = 40;
+    E->attack = 20;
+    E->defense = 20;
+    E->speed = 20;
+}
+
+// Gives the demon attributes
+void Demon(Enemy* E) {
+    strcpy(E->enemy, "Demon");
+    E->health = 50;
+    E->attack = 25;
+    E->defense = 25;
+    E->speed = 25;
+}
+
+void BattleLayout (Character* P, Enemy* E) {
+    
+
+    printf("                                Combat                                \n");
+    printf("|%-8s                         VS                    %8s: %s|\n", P->class, E->enemy, E->body);
+    printf("|-------------------------------------------------------------------|\n" );
+    printf("| Health: %3d                                           Health: %3d |\n", P->health, E->health);
+    printf("|                                                                   |\n");
+    printf("|                                                                   |\n");
+    printf("|       %s                                    %s               |\n", P->body1, E->body);
+    printf("|       %s                                                     |\n", P->body2);
+    printf("|       %s                                                     |\n", P->body3);
+    printf("|                                                                   |\n");
+    printf("|-------------------------------------------------------------------|\n" );
 }
 // Returns a value (min <= value <= max)
 int Dice(int min, int max) {
